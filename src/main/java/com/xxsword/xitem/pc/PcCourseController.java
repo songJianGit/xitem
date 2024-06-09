7b3f6e8a66c6a5abfadbe3ac2155671a636ae8df
songj <>
2024-06-09 15:34:53 +0800

    功能更新，bug修复。

--- src/main/java/com/xxsword/xitem/pc/PcCourseController.java
+++ src/main/java/com/xxsword/xitem/pc/PcCourseController.java
@@ -5,30 +5,18 @@ import com.xxsword.xitem.admin.constant.TimerType;
 import com.xxsword.xitem.admin.controller.BaseController;
 import com.xxsword.xitem.admin.domain.course.dto.CourseDto;
 import com.xxsword.xitem.admin.domain.course.entity.Course;
-import com.xxsword.xitem.admin.domain.course.entity.CourseFile;
-import com.xxsword.xitem.admin.domain.course.entity.CourseFileItem;
 import com.xxsword.xitem.admin.domain.course.vo.CoursePlayVO;
-import com.xxsword.xitem.admin.domain.exam.dto.ExamDto;
-import com.xxsword.xitem.admin.domain.exam.entity.Exam;
-import com.xxsword.xitem.admin.domain.exam.entity.UserPaper;
-import com.xxsword.xitem.admin.domain.exam.vo.UserPaperVO;
-import com.xxsword.xitem.admin.domain.system.entity.UserInfo;
-import com.xxsword.xitem.admin.service.course.CourseFileItemService;
 import com.xxsword.xitem.admin.service.course.CourseFileService;
 import com.xxsword.xitem.admin.service.course.CourseService;
-import com.xxsword.xitem.admin.utils.ExamUtil;
 import com.xxsword.xitem.admin.utils.Utils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -41,11 +29,15 @@ public class PcCourseController extends BaseController {
     private CourseFileService courseFileService;
 
     @RequestMapping("index")
-    public String index(Model model) {
-        CourseDto courseDto = new CourseDto();
+    public String index(CourseDto courseDto, Integer pageNum, Model model) {
+        if (pageNum == null) {
+            pageNum = 1;
+        }
         courseDto.setReleaseStatus(1);
-        List<Course> courseList = courseService.list(new Page<>(1, 12), courseDto.toQuery());
-        model.addAttribute("courseList", courseList);
+        Page<Course> coursePage = courseService.page(new Page<>(pageNum, 9), courseDto.toQuery());
+        model.addAttribute("courseList", coursePage.getRecords());
+        model.addAttribute("totalPages", Utils.getPage(coursePage.getTotal(), (int) coursePage.getSize()));
+        model.addAttribute("courseDto", courseDto);
         return "/pc/course/courseindex";
     }
 
--- src/main/resources/templates/pc/course/courseindex.ftl
+++ src/main/resources/templates/pc/course/courseindex.ftl
@@ -12,26 +12,57 @@
             margin-top: 7px;
             color: #000;
         }
+
+        .course-item {
+            padding: 0 0 0.25rem 0;
+        }
     </style>
 </head>
 <body>
 <div class="pc-main">
     <#include "../commons/header.ftl"/>
-    <div class="pc-body row">
-        <#list courseList as item>
-            <a class="col-4" href="${ctx.contextPath}/pc/course/c/${item.id!}">
-                <div class="card mt-3">
-                    <div class="card-body">
-                        <img style="width: 100%;max-height: 200px" src="${ctx.contextPath}${item.cover!'/static/admin/commons/img/defaultimg.webp'}" alt="图">
-                        <div class="course-title">${item.title!}</div>
+    <div class="pc-body">
+        <div class="input-group" style="max-width: 370px">
+            <input type="text" name="title" value="${courseDto.title!}" class="form-control" placeholder="请输入关键词">
+            <div class="input-group-append">
+                <button type="button" class="btn btn-outline-secondary" onclick="searchCourse()">搜索</button>
+            </div>
+        </div>
+        <div class="row">
+            <#list courseList as item>
+                <a class="col-4" href="${ctx.contextPath}/pc/course/c/${item.id!}">
+                    <div class="card mt-3">
+                        <div class="card-body course-item">
+                            <img style="width: 100%;max-height: 200px"
+                                 src="${ctx.contextPath}${item.cover!'/static/admin/commons/img/defaultimg.webp'}"
+                                 alt="图">
+                            <div class="course-title">${item.title!}</div>
+                        </div>
                     </div>
-                </div>
-            </a>
-        </#list>
+                </a>
+            </#list>
+        </div>
+        <div id="pageDiv" class="mt-3">
+        </div>
     </div>
 </div>
 <#include "../commons/js.ftl"/>
 <script type="text/javascript">
+    $(function () {
+        pageInit();
+    });
+
+    function pageInit() {
+        if ('${totalPages!}' != '0') {
+            paginator.init("pageDiv", `${totalPages!}`, "pageNum");
+            paginator.show(paginator.getUrlParam("pageNum"));
+        }
+    }
+
+    function searchCourse() {
+        let title = $("input[name='title']").val();
+        window.location.href = "${ctx.contextPath}/pc/course/index?title=" + title;
+    }
 </script>
 </body>
 </html>
