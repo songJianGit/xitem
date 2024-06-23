2fdbdacb186fa3777fd2b80d2eee9b4518f302f4
songj <>
2024-06-23 17:13:51 +0800

    功能更新，bug修复。

--- src/main/java/com/xxsword/xitem/pc/PcExamController.java
+++ src/main/java/com/xxsword/xitem/pc/PcExamController.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -222,7 +223,7 @@ public class PcExamController {
         }
     }
 
-    @RequestMapping("saveAnswer")
+    @PostMapping("saveAnswer")
     @ResponseBody
     public RestResult saveAnswer(HttpServletRequest request, String qid, String answers) {
         if (StringUtils.isBlank(answers)) {
