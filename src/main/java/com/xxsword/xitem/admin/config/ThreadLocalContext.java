a7e73027ca19d4ccbaedf10efbcb3f983f8a676c
sj <2443904383@qq.com>
2024-12-21 19:20:04 +0800

    注释添加

--- src/main/java/com/xxsword/xitem/admin/config/ThreadLocalContext.java
+++ src/main/java/com/xxsword/xitem/admin/config/ThreadLocalContext.java
@@ -7,6 +7,10 @@ import com.alibaba.ttl.TransmittableThreadLocal;
  */
 public class ThreadLocalContext {
     /**
+     * 该变量用于业务分表中，分表键的临时存储；
+     * <p>
+     * 其他业务如果需要，请另外再建；
+     * <p>
      * TransmittableThreadLocal 是阿里巴巴开源的 transmittable-thread-local 项目中的一个类。
      * 它扩展了 Java 标准库中的 ThreadLocal 和 InheritableThreadLocal，提供了更强大的功能，特别是在多线程环境和异步调用场景中
      */
